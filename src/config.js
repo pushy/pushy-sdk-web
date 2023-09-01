9c4af30f68581d1c806e2df8b03a86de31ba7079
Pushy <support@pushy.me>
2023-09-01 13:24:13 +0500

    1.0.11

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Web Push SDK version code
-    version: 1010,
+    version: 1011,
     // SDK platform
     platform: 'web',
     // Pushy API endpoints (production and development)
