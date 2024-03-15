e127b58badc8cd4a017581596a36d873cf70bd7e
Pushy <support@pushy.me>
2024-03-15 16:07:33 +0100

    1.0.18

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // Web Push SDK version code
-    version: 1017,
+    version: 1018,
     // SDK platform
     platform: 'web',
     // Pushy API endpoints (production and development)
