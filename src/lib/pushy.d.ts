002949a4f9d487c322c5c53418b89dfb1f0d1347
Pushy <support@pushy.me>
2024-08-14 10:22:59 -0600

    Add TypeScript declarations (#2) (thanks @sirkrisp)

---
+++ src/lib/pushy.d.ts
@@ -0,0 +1,11 @@
+declare module 'pushy-sdk-web' {
+    export function register(params: { appId: string }): Promise<string>;
+    export function setNotificationListener(callback: (data: any) => void): void;
+    export function subscribe(topics: string | string[]): Promise<void>;
+    export function unsubscribe(topics: string | string[]): Promise<void>;
+    export function isRegistered(): boolean;
+    export function validateDeviceCredentials(): Promise<void>;
+    export function isEnterpriseConfigured(): boolean;
+    export function setEnterpriseConfig(endpoint: string): void;
+    export function setProxyEndpoint(endpoint: string): void;
+}
